df2c95788e259f98b51f3f06a1bc03fa4d0f398c
Bhuvan <bhuvanchandranalabothu@gmail.com>
2026-04-12 12:56:46 +0530

    Practice problems week 7&8 level 3

--- Main.java
+++ Main.java
@@ -1,88 +1,83 @@
 /*
- * LEVEL 2 Practice Problems
- *
- * 1. Find factors of a number, store in array and compute:
- *    sum, product, sum of squares
- * 2. Sum of n natural numbers using recursion and formula
- * 3. Check whether a year is a Leap Year
+ * LEVEL 3 Practice Programs 
  */
-import java.util.Scanner;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        // ===== 1. Factors =====
-        System.out.print("Enter number: ");
-        int num = sc.nextInt();
+        // ===== 1. Football Heights =====
+        int[] h = new int[11];
+        Random r = new Random();
 
-        int[] factors = findFactors(num);
+        for (int i = 0; i < h.length; i++)
+            h[i] = 150 + r.nextInt(101);
 
-        int sum = 0;
-        long product = 1;
-        double sumSq = 0;
+        int sum = 0, min = h[0], max = h[0];
 
-        System.out.print("Factors: ");
-        for (int f : factors) {
-            System.out.print(f + " ");
-            sum += f;
-            product *= f;
-            sumSq += Math.pow(f, 2);
+        System.out.print("Heights: ");
+        for (int x : h) {
+            System.out.print(x + " ");
+            sum += x;
+            if (x < min) min = x;
+            if (x > max) max = x;
         }
 
-        System.out.println("\nSum: " + sum);
-        System.out.println("Product: " + product);
-        System.out.println("Sum of Squares: " + sumSq);
+        System.out.println("\nMean: " + (double) sum / h.length);
+        System.out.println("Shortest: " + min);
+        System.out.println("Tallest: " + max);
 
 
-        // ===== 2. Sum of n =====
-        System.out.print("\nEnter n: ");
+        // ===== 2. NumberChecker =====
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter number: ");
         int n = sc.nextInt();
 
-        if (n > 0) {
-            int r1 = recSum(n);
-            int r2 = n * (n + 1) / 2;
+        int[] d = NumberChecker.getDigits(n);
 
-            System.out.println("Recursion: " + r1);
-            System.out.println("Formula: " + r2);
+        System.out.println("Digits: " + d.length);
+        System.out.println("Duck: " + NumberChecker.isDuck(d));
+        System.out.println("Armstrong: " + NumberChecker.isArmstrong(n, d));
+
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int x : d) {
+            if (x > max1) { max2 = max1; max1 = x; }
+            else if (x > max2 && x != max1) max2 = x;
+
+            if (x < min1) { min2 = min1; min1 = x; }
+            else if (x < min2 && x != min1) min2 = x;
         }
 
-
-        // ===== 3. Leap Year =====
-        System.out.print("\nEnter year: ");
-        int year = sc.nextInt();
-
-        if (year >= 1582 && isLeap(year))
-            System.out.println("Leap Year");
-        else
-            System.out.println("Not Leap Year");
+        System.out.println("Largest: " + max1 + ", Second: " + max2);
+        System.out.println("Smallest: " + min1 + ", Second: " + min2);
 
         sc.close();
     }
+}
+// ===== Utility Class =====
+class NumberChecker {
 
-    // Find factors
-    static int[] findFactors(int n) {
-        int count = 0;
-        for (int i = 1; i <= n; i++)
-            if (n % i == 0) count++;
-
-        int[] arr = new int[count];
-        int j = 0;
-
-        for (int i = 1; i <= n; i++)
-            if (n % i == 0) arr[j++] = i;
-
+    static int[] getDigits(int n) {
+        String s = String.valueOf(Math.abs(n));
+        int[] arr = new int[s.length()];
+        for (int i = 0; i < s.length(); i++)
+            arr[i] = s.charAt(i) - '0';
         return arr;
     }
 
-    // Recursive sum
-    static int recSum(int n) {
-        if (n == 1) return 1;
-        return n + recSum(n - 1);
+    static boolean isDuck(int[] d) {
+        for (int x : d)
+            if (x == 0) return true;
+        return false;
     }
 
-    // Leap year
-    static boolean isLeap(int y) {
-        return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
+    static boolean isArmstrong(int n, int[] d) {
+        int sum = 0;
+        for (int x : d)
+            sum += (int) Math.pow(x, d.length);
+        return sum == Math.abs(n);
     }
 }
